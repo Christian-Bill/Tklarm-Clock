c58b723b4b2cbfc5d4025b8b15aecad3834cddad
BillT <billtorrentira@gmail.com>
2021-12-04 00:19:42 +0800

    Add treeview for alarm schedules

--- view.py
+++ view.py
@@ -1,11 +1,10 @@
-# Views for the main app
 import tkinter as tk
 from controls import *
 from tkinter import ttk
 
 
 class View():
-    '''Visual components of the app'''
+    '''Visual components of Tklarm Clock'''
 
     def __init__(self):
 
@@ -13,7 +12,10 @@ class View():
     
     def main_window(self):
         self.root = tk.Tk()
+        self.root.geometry('426x562')
+        self.root.resizable(0, 0)
         self.app_components()
+        # self.root.update()
         tk.mainloop()
 
     def app_components(self):
@@ -23,41 +25,103 @@ class View():
 
     def widgets(self):
         self.current_time_label()
+        self.hour_event_entries()
         self.add_delete_btns()
+        self.schedules()
 
     def current_time_label(self):
-        '''Create current time label'''
-        self.time_frame = ttk.Frame(self.root, style="time.TFrame",)
+        '''Create Current Time Label'''
+        self.time_frame = ttk.Frame(self.root, style="time.TFrame")
         self.create_time_label()
-        self.time_frame.pack(side='top', fill='x', expand=True)
+        self.time_frame.pack(side='top', fill='both')
 
     def create_time_label(self):
         time_label = ttk.Label(self.time_frame, font=("calibri", 35, "bold"),
-                            width=14, anchor="center", background="black",
+                            anchor="center", background="black",
                             foreground="white")
-        time_label.pack(side='left', fill='x', expand=True, pady=10)
+        time_label.pack(side='top', fill='both', expand=True, pady=10)
         fetch_current_time(time_label)
 
+    def hour_event_entries(self):
+        '''Create Hour and Event Entries'''
+        self.entry_frame = ttk.Frame(self.root, height='5', style="entries.TFrame")
+        self.create_hour_entry()
+        self.create_event_entry()     
+        self.entry_frame.pack(side='top', fill='both')
+
+    def create_hour_entry(self):
+        self.hour_labelframe = ttk.LabelFrame(self.entry_frame, text='Hour', style="hour.TLabelframe")
+        search_entry = ttk.Entry(self.hour_labelframe)
+        search_entry.pack(side='left', fill='both', expand=True, pady=5, padx=5)
+        self.hour_labelframe.pack(side='left', fill='both', expand=True, pady=5, padx=5)
+
+    def create_event_entry(self):
+        self.event_frame = ttk.LabelFrame(self.entry_frame, text='Event', style="event.TLabelframe")
+        event_entry = ttk.Entry(self.event_frame)
+        event_entry.pack(side='left', fill='both', expand=True, pady=5, padx=5)
+        self.event_frame.pack(side='left', fill='both', expand=True, pady=5, padx=5)
+
     def add_delete_btns(self):
-        '''Create Add and Delete labels'''
-        self.btns_frame = ttk.Frame(self.root, style="btn.TFrame",)
+        '''Create Add and Delete Buttons'''
+        self.btns_frame = ttk.Frame(self.root, height='5', style="entries.TFrame")
+        self.create_btn_labelframe()
+        self.btns_frame.pack(side='top', fill='both')
+
+    def create_btn_labelframe(self):
+        self.btns_labelframe = ttk.LabelFrame(self.btns_frame, text='Controls', style="btn.TLabelframe")
         self.display_buttons()
-        self.btns_frame.pack(side='top', fill='x', expand=True)
+        self.btns_labelframe.pack(side='top', fill='both', expand=True, pady=5, padx=5)
 
     def display_buttons(self):
         self.create_add_button()
         self.create_del_button()
 
     def create_add_button(self):
-        add_btn = ttk.Button(self.btns_frame, text='+', style="btn.TButton")
-        add_btn.pack(side='left', fill='x', expand=True)
+        add_btn = ttk.Button(self.btns_labelframe, text='+', style="btn.TButton")
+        add_btn.pack(side='left', fill='both', expand=True, pady=5, padx=5)
 
     def create_del_button(self):
-        del_btn = ttk.Button(self.btns_frame, text='-', style="btn.TButton")
-        del_btn.pack(side='left', fill='x', expand=True)
+        del_btn = ttk.Button(self.btns_labelframe, text='-', style="btn.TButton")
+        del_btn.pack(side='left', fill='both', expand=True, pady=5, padx=5)
 
     def schedules(self):
-        pass
+        '''Create Schedules Treeview'''
+        self.sched_frame = ttk.Frame(self.root, style="sched.TFrame")
+        self.create_sched_labelframe()
+        self.sched_frame.pack(side='bottom', fill='both', expand=True)
+        self.sched_tree.bind('<Button-1>', self.block_columns_resize)
+
+    def create_sched_labelframe(self):
+        self.sched_labelframe = ttk.LabelFrame(self.sched_frame, text='Schedule', style="sched.TLabelframe")
+        self.create_sched_tree()
+        self.create_tree_scroll()
+        self.sched_labelframe.pack(fill='both', expand=True, pady=5, padx=5)
+
+    def create_sched_tree(self):
+        self.sched_tree = ttk.Treeview(self.sched_labelframe, show="headings", columns=('Hour, Event'), height=15)
+        self.create_columns()
+        self.sched_tree.pack(side='left', fill='both', expand=True, pady=5, padx=5)
+
+    def create_columns(self):
+        self.create_first_col()
+        self.create_second_col()
+
+    def create_first_col(self):
+        self.sched_tree.column('#1')
+        self.sched_tree.heading('#1', text='Hour', anchor='center')
+
+    def create_second_col(self):
+        self.sched_tree.column('#2')
+        self.sched_tree.heading('#2', text='Event', anchor='center')
+
+    def block_columns_resize(self, event):
+        if self.sched_tree.identify_region(event.x, event.y) == "separator":
+            return "break"
+    
+    def create_tree_scroll(self):
+        tree_scroll = ttk.Scrollbar(self.sched_tree, orient="vertical", command=self.sched_tree.yview)
+        tree_scroll.pack(side='right', fill='y')
+        self.sched_tree.configure(yscrollcommand=tree_scroll.set)
 
     def styles(self):
         self.style = ttk.Style()
