59ca2b6e30c62c6701f4515c179aa251db66da23
BillT <billtorrentira@gmail.com>
2021-12-08 23:03:01 +0800

    Import Model for backend process

--- controller.py
+++ controller.py
@@ -1,14 +1,19 @@
-# from model import Model
 from view import View
+from model import Model
+
 
 class Controller:
+    
     def __init__(self):
 
-        self.view = View(self)
-        # self.model = Model(self)
+        self.model = Model()
+        self.view = View(self)        
 
     def main(self):
         self.view.main()
+
+    def fetch_current_time(self, time_label):
+        self.model.current_time_running(time_label)       
 
 
 if __name__ == '__main__':
